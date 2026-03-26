d43c76c11b5c8f2127c3704596de355576c68f75
Kathryn Huxtable <kathryn@kathrynhuxtable.org>
2026-03-26 11:39:49 -0500

    Removed some dev code

--- src/main/java/org/kathrynhuxtable/radiofreelawrence/game/RadioactiveFreeLawrenceApplication.java
+++ src/main/java/org/kathrynhuxtable/radiofreelawrence/game/RadioactiveFreeLawrenceApplication.java
@@ -33,14 +33,6 @@ public class RadioactiveFreeLawrenceApplication {
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return args -> {
 
-//			System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//			String[] beanNames = ctx.getBeanDefinitionNames();
-//			Arrays.sort(beanNames);
-//			for (String beanName : beanNames) {
-//				System.out.println(beanName);
-//			}
-
 			gameContext.gameNode.createDefaultElements();
 
 			GameVisitor visitor = new GameVisitor(gameContext.gameNode, gameContext.errorReporter);
